0ca5c1dd5cf8ea32fb0622b58094b1f25b684d2b
unibosy <unibosy@gmail.com>
2017-11-29 10:08:03 +0800

    test call wrapper dynamic library done

--- native_layer/cppwrapper/samples/testCallLibrary.cpp
+++ native_layer/cppwrapper/samples/testCallLibrary.cpp
@@ -4,8 +4,9 @@
 //int test(int argc, char * const argv[]);
 int main()
 {
-	int argc = 10;
-  char* const argv[] = {"1","2","3","4"};
+	int argc = 13;
+  char* const argv[] = {"./recorder_local","--recordFileRootDir","./","--channel","video","--uid","0","--appliteDir","/home/wangshiwei/ars/code_ars/javaDemo/native_layer/cppwrapper/bin","--idle","20","--appId"
+		,"0c0b4b61adf94de1befd7cdd78a50444"};
 	totalFunc(argc, argv);
 	return 0;	
 }
--- test/callLibrary/main.cpp
+++ test/callLibrary/main.cpp
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "shared.h"
-
 int main()
 {
 	shared_print();
